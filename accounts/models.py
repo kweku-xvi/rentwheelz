22099065a77a3d0446c374362206e6f3bb4b7087
kweku-xvi <nksarps@gmail.com>
2024-02-07 10:46:51 +0000

    added users model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,58 @@
+import uuid
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self,email,password:None,**extra_fields):
+        if not email:
+            raise ValueError(_("The email is Required"))
+        email = self.normalize_email(email=email)
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+
+    def create_superuser(self,email,password=None,**extra_fields):
+        extra_fields.setdefault("is_staff",True)
+        extra_fields.setdefault("is_superuser",True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("SuperUser must have is_staff=True"))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("SuperUser must have is_superuser=True"))
+        
+        return self.create_user(email,password,**extra_fields)
+
+
+class User(AbstractUser):
+    id = models.CharField(max_length=8, primary_key=True, unique=True)
+    name = models.CharField(max_length=255)
+    gender = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=50, unique=True)
+    phone_number = models.CharField(max_length=13, unique=True)
+    dob = models.DateField(blank=False, null=False)
+    address = models.CharField(max_length=255)
+    license_number = models.CharField(max_length=50, unique=True)
+    is_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    objects = CustomUserManager()
+
+    REQUIRED_FIELDS = ['name', 'gender', 'username', 'phone_number', 'dob', 'address', 'license_number']
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.username
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.id = str(uuid.uuid4())[:8]
+        super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ('-created_at',)
+
+    
