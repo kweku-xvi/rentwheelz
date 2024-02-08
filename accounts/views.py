727def4af8fe027c0279c64bddc99f017184d2c4
kweku-xvi <nksarps@gmail.com>
2024-02-08 09:57:34 +0000

    added views for sign up and user verification

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,101 @@
+from .models import User
+from .permissions import IsVerified
+from .serializers import SignUpSerializer
+from .utils import send_verification_email, send_password_reset_email
+import jwt, os
+from django.contrib.sites.shortcuts import get_current_site
 from django.shortcuts import render
+from django.urls import reverse
+from dotenv import load_dotenv
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 
-# Create your views here.
+load_dotenv()
+
+
+@api_view(['POST'])
+def sign_up_view(request):
+    if request.method == 'POST':
+        serializer = SignUpSerializer(data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+
+            user = User.objects.get(email=serializer.validated_data['email'])
+            token = RefreshToken.for_user(user)
+            current_site = get_current_site(request).domain
+            relative_link = reverse('verify_user')
+            absolute_url = f'http://{current_site}{relative_link}?token={token}'
+            link = str(absolute_url)
+            send_verification_email(username=user.username, email=user.email, link=link)
+
+            return Response(
+                {
+                    'success':True,
+                    'user':serializer.data
+                }, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            {
+                'success':False,
+                'user':serializer.errors
+            }, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+@api_view(['GET'])
+def verify_user_view(request):
+    if request.method == 'GET':
+        token = request.GET.get('token')
+
+        try:
+            payload = jwt.decode(token, os.getenv('SECRET_KEY'), algorithms=['HS256'])
+            user = User.objects.get(id=payload['user_id'])
+
+            if not user.is_verified:
+                user.is_verified = True
+                user.save()
+            
+            return Response(
+                {
+                    'success':True,
+                    'message':'User has been successfully verified.'
+                }, status=status.HTTP_200_OK
+            )
+        except jwt.ExpiredSignatureError as e:
+            return Response(
+                {
+                    'success':False,
+                    'message':'Activation link expired.'
+                }, status=status.HTTP_400_BAD_REQUEST
+            )
+        except jwt.exceptions.DecodeError as e:
+            return Response(
+                {
+                    'success':False,
+                    'message':'Invalid token.'
+                }, status=status.HTTP_400_BAD_REQUEST
+            )
+        except jwt.exceptions.InvalidTokenError as e:
+            return Response(
+                {
+                    'success':False,
+                    'message':'Invalid token.'
+                }, status=status.HTTP_400_BAD_REQUEST
+            )
+        except User.DoesNotExist as e:
+            return Response(
+                {
+                    'success':False,
+                    'message':'User does not exist'
+                }, status=status.HTTP_404_NOT_FOUND
+            )
+        except Exception as e:
+            return Response(
+                {
+                    'success':False,
+                    'message':str(e)
+                }, status=status.HTTP_400_BAD_REQUEST
+            )
