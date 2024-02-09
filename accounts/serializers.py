dab8fc1864e45f3e8246c6f7032fe094927503d1
kweku-xvi <nksarps@gmail.com>
2024-02-09 11:35:51 +0000

    added login and user info serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,7 @@
 from .models import User
+from django.contrib.auth import authenticate
 from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class SignUpSerializer(serializers.ModelSerializer):
@@ -40,3 +42,37 @@ class SignUpSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=50, min_length=8, write_only=True)
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        user = authenticate(username=email, password=password)
+
+        if not user:
+            raise serializers.ValidationError('Invalid credentials. Try again.', code='authentication')
+
+        attrs['user'] = user
+        return attrs
+
+    def generate_jwt_tokens(self, attrs):
+        user = attrs.get('user')
+        refresh = RefreshToken.for_user(user)
+
+        tokens = {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token)
+        }
+
+        return tokens
+
+
+class UserInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'name', 'gender', 'email', 'username', 'dob', 'address', 'phone_number', 'license_number']
