3ab44cb8038436bff5c8374c9386ddd8073047d3
kweku-xvi <nksarps@gmail.com>
2024-02-09 11:38:30 +0000

    admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,5 +6,4 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ['id', 'username', 'email', 'phone_number', 'license_number']
     readonly_fields = ('created_at',)
 
-
 admin.site.register(User, UserAdmin)
