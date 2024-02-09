5be9fbb8852464640b74096c99260e69f1c69126
kweku-xvi <nksarps@gmail.com>
2024-02-09 11:36:37 +0000

    added url endpoints for user login, password reset and user search

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,10 @@ from django.urls import path
 urlpatterns = [
     path('signup', views.sign_up_view, name='sign_up'),
     path('verify-user', views.verify_user_view, name='verify_user'),
+    path('login', views.login_view, name='login'),
+    path('password-reset', views.password_reset_view, name='password_reset'),
+    path('password-reset-confirm', views.password_reset_confirm_view, name='password_reset_confirm'),
+    path('u/all', views.get_all_users_view, name='get_all_users'),
+    path('u/search', views.search_users_view, name='search_user'),
+    path('u/<str:uid>', views.get_specific_user_view, name='get_user'),
 ]
